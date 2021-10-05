059943f06972c5b9539c23880dcdc5a51dd677af
Leticia Araujo <51300153+araujoleticiafb@users.noreply.github.com>
2021-10-05 20:31:05 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ if [ $@ = "local" ]
     allure generate allure-results
 else
   echo "Starting tests..."
+  echo -e 'pcm.!default {\n type hw\n card 0\n}\n\nctl.!default {\n type hw\n card 0\n}' > ~/.asoundrc
   npx cypress run --env allure=true,allureResultsPath=allure-results
   allure serve -p 8787 /allure-results
 fi
